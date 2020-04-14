1f828e084ab40f927819e21b0870efde4e54acfc
eNd_SHA <end_sha@eNd-SHAs-MacBook-Pro.local>
2020-04-14 23:30:31 +0700

    Prototype Build

---
+++ precache-manifest.efc37ea8bbedf897a6fd5f43b24b5a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad648a54d50bbe8ea1513f28c492de2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0155ca5402b6062b81d8",
+    "url": "/static/css/main.8f1f9afc.chunk.css"
+  },
+  {
+    "revision": "5a8dd8cd3ef95da36e19",
+    "url": "/static/js/2.5aaa9621.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5aaa9621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0155ca5402b6062b81d8",
+    "url": "/static/js/main.a65d1dc4.chunk.js"
+  },
+  {
+    "revision": "905e4d6bd7d9946d806f",
+    "url": "/static/js/runtime-main.8ab15afb.js"
+  },
+  {
+    "revision": "194011ea1ed42cd52cf5eeac69859a54",
+    "url": "/static/media/test_2.194011ea.png"
+  }
+]);
